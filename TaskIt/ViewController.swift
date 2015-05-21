3becea6de1daf3798101eef03c72832c51dbd9d7
Jim Veneskey <github@strifecrafter.com>
2015-05-21 12:29:07 -0400

    Added a connection/outlet between the storyboard table and the viewcontroller

--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
