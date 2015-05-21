2548a10a8bf2b41557a844b8acda5df7428602d6
Jim Veneskey <github@strifecrafter.com>
2015-05-21 12:40:53 -0400

    Created a new class to control the prototype cell, and got rid of that pesky compiler warning

---
+++ TaskIt/TaskCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskCell.swift
+//  TaskIt
+//
+//  Created by jim Veneskey on 5/21/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
