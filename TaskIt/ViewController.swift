0f67f5d0d046419cfa22b1bdfbb344cf92dda7b7
Jim Veneskey <github@strifecrafter.com>
2015-05-21 12:53:39 -0400

    Giving the cells a bit more functionality - being able to select them now

--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -32,12 +32,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     // UITableViewDataSource (required methods)
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 0
+        return 5
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        return UITableViewCell()
+        println("Row: \(indexPath.row)")
+        
+        var cell: TaskCell = tableView.dequeueReusableCellWithIdentifier("myCell") as! TaskCell
+        return cell
     }
     
     // UITableViewDelegate required method
