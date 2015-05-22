3283ee576416ca349c0aa2d265ccc10a52472e78
Jim Veneskey <github@strifecrafter.com>
2015-05-22 12:48:30 -0400

    Starting the process of switching from dictionaries to structs

---
+++ TaskIt/TaskModel.swift
@@ -0,0 +1,16 @@
+//
+//  TaskModel.swift
+//  TaskIt
+//
+//  Created by jim Veneskey on 5/22/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+import Foundation
+
+struct TaskModel {
+    var task: String
+    var subTask: String
+    var date: String
+    
+}
