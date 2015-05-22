3d1f9c6a9176619da240158dffc2195d746daff7
Jim Veneskey <github@strifecrafter.com>
2015-05-22 12:44:13 -0400

    Created a dictionary to use as a data source for the table cells

--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     @IBOutlet weak var tableView: UITableView!
     
+    var taskArray:[Dictionary<String, String>] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,6 +23,22 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // example of how you could access these connection via code
         // self.tableView.dataSource = self
         // self.tableView.delegate = self
+        
+        // Create example tasks
+        let task1:Dictionary<String, String> = ["task": "Study French", "subtask": "Verbs", "date": "01/14/2014"]
+        //println("task1 - task: \(task1["task"])")
+        println(task1["task"])
+        println(task1["subtask"])
+        println(task1["date"])
+        
+        let task2:Dictionary<String, String> = ["task": "Eat Dinner", "subtask": "Burgers", "date": "01/14/2014"]
+        
+        let task3:Dictionary<String, String> = ["task": "Gym", "subtask": "Leg day", "date": "01/14/2014"]
+
+        taskArray = [task1, task2, task3]
+        
+        // refresh the information in the tableview - which calls both ancillary functions
+        self.tableView.reloadData()
         
     }
 
@@ -32,18 +50,20 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     // UITableViewDataSource (required methods)
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 5
+        return taskArray.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         println("Row: \(indexPath.row)")
         
+        let taskDict = taskArray[indexPath.row]
+        
         var cell: TaskCell = tableView.dequeueReusableCellWithIdentifier("myCell") as! TaskCell
         
-        cell.taskLabel.text = "Study German"
-        cell.descriptionLabel.text = "Verbs in past and present"
-        cell.dateLabel.text = "01/01/1970"
+        cell.taskLabel.text = taskDict["task"]
+        cell.descriptionLabel.text = taskDict["subtask"]
+        cell.dateLabel.text = taskDict["date"]
         
         return cell
     }
