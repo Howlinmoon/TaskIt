9834bc89705e8aa87d8b8ecf95aa0041815f2eda
Jim Veneskey <github@strifecrafter.com>
2015-05-22 13:09:41 -0400

    Added a 2nd view controller, and created the segue for it

---
+++ TaskIt/TaskDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TaskDetailViewController.swift
+//  TaskIt
+//
+//  Created by jim Veneskey on 5/22/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+import UIKit
+
+class TaskDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
